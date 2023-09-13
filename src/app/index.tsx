e55f6248da10eb2c74509b5ca24a14c053429090
Hashith Karunarathne <hashithkarunarathne@live.com>
2023-09-13 11:19:44 +0800

    Added svg icon index - via web interface

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import IconStackOverflow from './IconStackOverflow';
+import IconBxlGithub from './IconBxlGithub';
+import IconLogoDocker from './IconLogoDocker';
+import IconLogoNpm from './IconLogoNpm';
+import IconBxlLinkedinSquare from './IconBxlLinkedinSquare';
+import IconMedium from './IconMedium';
+import IconEmailArrowRightOutline from './IconEmailArrowRightOutline';
+
+export {
+  IconStackOverflow,
+  IconBxlGithub,
+  IconLogoDocker,
+  IconLogoNpm,
+  IconBxlLinkedinSquare,
+  IconMedium,
+  IconEmailArrowRightOutline,
+};
